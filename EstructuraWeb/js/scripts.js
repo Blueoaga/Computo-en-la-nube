38d416c53b346070aef5a00c3a211c376b5c222c
Omar Antonio Gutiérrez Amillano <oagamon140203@gmail.com>
2025-09-09 08:51:00 -0700

    Add files via upload

---
+++ EstructuraWeb/js/scripts.js
@@ -0,0 +1,25 @@
+/*imprimir lista de nombres
+crea un  programa en javascript que cumpla 
+lo siguiente:
+
+1.Define un objeto llamado grupo.
+   este objeto debe tener lista de 10 nombres.
+2.Crea una funcion llamda imprimirNombres.
+    Esta funcion debe recorrer la lista de nombres y mostrarlos
+    en la consola uno a uno.
+3.Llama a la funcion imprimirNombres para mostrar todos los nombres.*/
+
+    let grupo =[{Nombre:"Juan",Apellido:"Perez"},
+                {Nombre:"Pedro",Apellido:"Gomez"},
+                {Nombre:"Luis",Apellido:"Won"},
+                {Nombre:"Francisco",Apellido:"Hi"},
+                {Nombre:"Isela",Apellido:"Matinez"},
+                {Nombre:"Ana",Apellido:"Lopez"},
+                {Nombre:"Julia",Apellido:"Hernandez"},
+                {Nombre:"Juana",Apellido:"Velazquez"},
+                {Nombre:"Martin",Apellido:"Martin"},
+                {Nombre:"Maria",Apellido:"Guerra"}]
+
+  function imprimirNombres (grupo){
+    
+  }                
